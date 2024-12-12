85741b3c237263fe4b1eb721f2ebb74dd390d4cb
Pierre Burnn <100449580+pierreburnn@users.noreply.github.com>
2024-12-12 11:23:16 +0100

    Programme1.py

---
+++ Questions/Programme1.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+#on ouvre ici le fichier ics
+def lire_fichier_ics(chemin_fichier):
+    with open(chemin_fichier, 'r', encoding='utf-8') as fichier:
+        return fichier.readlines()
+
+
+def extraire_evenement_ics(contenu_ics):
+
+    evenement = {}
+    for ligne in contenu_ics:
+        if ligne.startswith('UID:'):
+            evenement['uid'] = ligne.split(':', 1)[1].strip()
+        elif ligne.startswith('DTSTART:'):
+            evenement['date_debut'] = ligne.split(':', 1)[1].strip()
+        elif ligne.startswith('DTEND:'):
+            evenement['date_fin'] = ligne.split(':', 1)[1].strip()
+        elif ligne.startswith('SUMMARY:'):
+            evenement['intitule'] = ligne.split(':', 1)[1].strip()
+        elif ligne.startswith('LOCATION:'):
+            evenement['salle'] = ligne.split(':', 1)[1].strip()
+        elif ligne.startswith('DESCRIPTION:'):
+            evenement['description'] = ligne.split(':', 1)[1].strip()
+    return evenement
+
+
+
+def convertir_vers_pseudo_csv(evenement):
+
+    date = datetime.strptime(evenement['date_debut'][:8], '%Y%m%d').strftime('%d-%m-%Y')
+    heure_debut = datetime.strptime(evenement['date_debut'][9:15], '%H%M%S').strftime('%H:%M')
+    heure_fin = datetime.strptime(evenement['date_fin'][9:15], '%H%M%S')
+    heure_debut_dt = datetime.strptime(evenement['date_debut'][9:15], '%H%M%S')
+    duree = heure_fin - heure_debut_dt
+    duree_formatee = f"{duree.seconds // 3600:02}:{(duree.seconds // 60) % 60:02}"
+
+    groupes = "|".join(evenement['description'].split('\\n')) if 'description' in evenement else "vide"
+    return f"{evenement['uid']};{date};{heure_debut};{duree_formatee};CM;{evenement['intitule']};{evenement['salle']};LACAN DAVID;S1"
+
+def main():
+    chemin_fichier = "/Users/pierrehiltenbrand/Downloads/evenementSAE_15.ics"  # Remplacez par le chemin de votre fichier
+    contenu_ics = lire_fichier_ics(chemin_fichier)
+    evenement = extraire_evenement_ics(contenu_ics)
+    pseudo_csv = convertir_vers_pseudo_csv(evenement)
+    print(pseudo_csv)
+
+if __name__ == "__main__":
+    main()
